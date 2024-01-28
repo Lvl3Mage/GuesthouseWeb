db55ee8c965d7550e39219a6c6e1fc014a9af677
Lvl3Mage <lvlthreemage@gmail.com>
2024-01-28 16:49:35 +0000

    Deploying to gh-pages from @ Lvl3Mage/GuesthouseWeb@3f23d5e1a52ac1585759abe21d045fc64fcbc751 🚀

--- js/main.js
+++ js/main.js
@@ -172,6 +172,57 @@ $(document).on("click", "[data-click-selectable]", function () {
   }
   $(this).addClass(cssClass);
 });
+$(document).ready(function () {
+  $(".image-slider-modal").slick({
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    adaptiveHeight: true,
+    prevArrow: `<div class='z-10 cursor-pointer absolute left-0.5 h-full m-auto flex justify-center items-center'>
+	        <div class="bg-black/50 rounded-full w-8 h-8 flex justify-center items-center text-white text-xl">
+	        	<i class='fa-solid fa-angle-left'></i>
+
+	        </div>
+        </div>`,
+    nextArrow: `<div class='z-10 cursor-pointer absolute right-0.5 h-full m-auto flex justify-center items-center'>
+	        <div class="bg-black/50 rounded-full w-8 h-8 flex justify-center items-center text-white text-xl">
+	        	<i class='fa-solid fa-angle-right'></i>
+
+	        </div>
+        </div>`
+  });
+  $(".image-preview-slider").each(function () {
+    let main = $(this).find(".image-preview-slider-main")[0];
+    let nav = $(this).find(".image-preview-slider-nav")[0];
+    $(main).slick({
+      dots: false,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: nav
+    });
+    $(nav).slick({
+      dots: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: `<div class='z-10 cursor-pointer absolute left-0.5 h-full m-auto flex justify-center items-center'>
+		        <div class="bg-black/50 rounded-full w-5 h-5 flex justify-center items-center text-white text-base">
+		        	<i class='fa-solid fa-angle-left'></i>
+
+		        </div>
+	        </div>`,
+      nextArrow: `<div class='z-10 cursor-pointer absolute right-0.5 h-full m-auto flex justify-center items-center'>
+		        <div class="bg-black/50 rounded-full w-5 h-5 flex justify-center items-center text-white text-base">
+		        	<i class='fa-solid fa-angle-right'></i>
+
+		        </div>
+	        </div>`,
+      asNavFor: main
+    });
+  });
+});
 
 /***/ }),
 
